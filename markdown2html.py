ca004e07ad4cc74dabbc257644632610f1fa266b
joseftango <4279@holbertonschool.com>
2023-08-24 15:06:26 +0100

    markdown2html.py

---
+++ markdown2html.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""create a markdown2html.py that takes two arguments"""
+from sys import argv, stderr, exit
+from os.path import exists
+from markdown import markdown
+
+
+if __name__ == '__main__':
+    num_of_arg = len(argv[1:])
+    if num_of_arg < 2:
+        stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+        exit(1)
+    if not exists(f'{argv[1]}'):
+        stderr.write(f'Missing {argv[1]}\n')
+        exit(1)
+    else:
+        markdown_file = argv[1]
+        html_file = argv[2]
+        with open(markdown_file, "r") as file:
+            content = file.read()
+            html = markdown(content)
+        with open(html_file, 'w') as f:
+            f.write(html + '\n')
